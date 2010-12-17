70bff4406622fcfdc27f4fb426df6d1e62fdda22
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2010-12-17 19:06:28 +0100

    Test d'article

---
+++ tests/test/ArticleMock.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.ArrayList;
+
+import stock.Article;
+import stock.Component;
+/**
+ * This class is aimed to test every Article method, to ensure
+ * the Article's children classes won't have to test each.
+ * @author Guy Champollion
+ * @see Article
+ */
+public class ArticleMock extends Article {
+    /**
+     * The constructor of the Article mock. 
+     * @param nbInStock How many of the given Article are already available in stock.
+     */
+    public ArticleMock(int nbInStock) {
+        super(nbInStock);
+        this.price = 0;
+    }
+    /**
+     * Get all of the Article's component.
+     * This mock doesn't contain any.
+     */
+    @Override
+    public ArrayList<Component> getComponents() {
+        return new ArrayList<Component>();
+    }
+
+}
---
+++ tests/test/ArticleTest.java
@@ -0,0 +1,59 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import stock.NotAvailableException;
+/**
+ * This is a JUnit test class to check the Article behaviour using a mock : ArticleMock.
+ * @author Guy Champollion
+ * @see ArticleMock
+ * @see Article
+ */
+public class ArticleTest {
+    private ArticleMock article;
+    public static final int NUMBERINSTOCK = 1;
+    @Before
+    public void setUp() throws Exception {
+        article = new ArticleMock(NUMBERINSTOCK);
+    }
+    /**
+     * Chech every attributes that have been instantiated by the Article constructor.
+     */
+    @Test
+    public void testArticle() {
+        // Check the number of Article available
+        assertEquals(NUMBERINSTOCK,article.getInStock());
+        // Check the number of Article sold, when no Article has been sold
+        assertEquals(0,article.getSold());
+    }
+    /**
+     * Check the effect of one sell.
+     */
+    @Test
+    public void testSell() throws NotAvailableException {
+        article.sell();
+        assertEquals(0,article.getInStock());
+        assertEquals(1,article.getSold());
+        assertTrue(article.getPrice() == article.soldValue());
+    }
+    /**
+     * Check the effect of an impossible sell.
+     */
+    @Test(expected=NotAvailableException.class)
+    public void testFailedSell() throws NotAvailableException {
+        article.sell();
+        article.sell();
+        article.sell();
+    }
+    /**
+     * Check the total value when none has been sold.
+     */
+    @Test
+    public void testSoldValue(){
+        assertTrue(0 == article.soldValue());
+    }
+
+}
