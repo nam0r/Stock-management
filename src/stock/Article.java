cd107e6d48e1f188871796ceff89114820c57fc1
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2010-12-17 16:04:01 +0100

    Article anglisé

--- src/stock/Article.java
+++ src/stock/Article.java
@@ -1,11 +1,16 @@
 package stock;
-
+/**
+ * This is an abstract class for all the Article that are sold.
+ * @author Guy Champollion
+ * @author Roman Mkrtchian
+ *
+ */
 public abstract class Article {
-    /** Le prix de l'article, en euros. */
-    protected double prix;
-    /** Le nombre d'article encore disponible en stock. */
-    protected int nbEnStock;
-    /** Le nombre d'article vendu. */
-    protected int nbVendus;
+    /** The price of the Article, in euro. */
+    protected double price; // in euro
+    /** The number of Articles available in stock. */
+    protected int nbInStock;
+    /** The number of Articles that have been sold. */
+    protected int nbSold;
 
 }
