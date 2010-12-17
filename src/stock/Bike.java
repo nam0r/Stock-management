8141d52cc525e25d8ab550d7c2620ca6ba8b79d9
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2010-12-17 17:31:43 +0100

    Bike modifié, components un peu refactorisés

--- src/stock/Bike.java
+++ src/stock/Bike.java
@@ -9,17 +9,27 @@ import java.util.ArrayList;
  */
 public class Bike extends Article {
     Brakes brakes;
-    Forks forks;
-    Frames frames;
-    Suspensions suspensions;
+    Fork fork;
+    Frame frame;
+    Suspension suspension;
     Wheels wheels;
-    
-    public Bike(int nbInStock, Brakes brakes, Forks forks, Frames frames, Suspensions suspensions, Wheels wheels){
+    /**
+     * The constructor for the Bike class. It determines the Bike components,
+     *   how many of this Bike have been sold or are available,
+     *   and then find the price of the Bike.
+     * @param nbInStock  The number of this new Bike in stock.
+     * @param brakes     The Brakes model of the Bike.
+     * @param fork       The Fork model of the Bike.
+     * @param frame      The Frame model of the Bike.
+     * @param suspension The Suspension model of the Bike.
+     * @param wheels     The Wheels model of the Bike.
+     */
+    public Bike(int nbInStock, Brakes brakes, Fork fork, Frame frame, Suspension suspension, Wheels wheels){
         super(nbInStock);
         this.brakes = brakes;
-        this.forks = forks;
-        this.frames = frames;
-        this.suspensions = suspensions;
+        this.fork = fork;
+        this.frame = frame;
+        this.suspension = suspension;
         this.wheels = wheels;
         
         this.price = 0;
@@ -27,14 +37,30 @@ public class Bike extends Article {
             this.price += c.getPrice();
         }
     }
-    
+    /**
+     * A method to get the Bike's components and returns a list of Components.
+     * @return list An ArrayList of Components.
+     */
     public ArrayList<Component> getComponents(){
         ArrayList<Component> list = new ArrayList<Component>();
         list.add(brakes);
-        list.add(forks);
-        list.add(frames);
-        list.add(suspensions);
+        list.add(fork);
+        list.add(frame);
+        list.add(suspension);
         list.add(wheels);
         return list;
+    }
+    /**
+     * The toString method that returns a String about the current Bike.
+     * @return string A String describing the bike.
+     */
+    public String toString(){
+        String string="";
+        string+="Frame: "     +frame.getName()     +"\n";
+        string+="Fork: "      +fork.getName()      +"\n";
+        string+="Suspension: "+suspension.getName()+"\n";
+        string+="Wheels: "    +wheels.getName()    +"\n";
+        string+="Brakes: "    +brakes.getName()    +"\n";
+        return string;
     }
 }
