aca114fe9cf8041a0afc35c801c1cfccf3764d36
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2010-12-17 20:43:59 +0100

    View

--- src/view/Terminal.java
+++ src/view/Terminal.java
@@ -15,9 +15,45 @@ public class Terminal {
         this.data=data;
     }
     /**
-     * 
+     * Displays the menu and the available commands.
      */
     public void launchMenu(){
         
+        int choice;
+        do {
+             System.out.println();
+             System.out.println("      **************************************");
+             System.out.println("      *         Stock Management           *");
+             System.out.println("      **************************************");
+             System.out.println("      * 1- Displays inventory              *");
+             System.out.println("      * 2- A product is sold               *");
+             System.out.println("      *                                    *");
+             System.out.println("      * 0- Quit                            *");
+             System.out.println("      **************************************");
+             System.out.println();
+             do{
+                 System.out.print("      Your choice : ");
+                 choice = IO.readInt();
+             } while(choice != 1 && choice != 2 && choice != 0);
+                 
+
+             switch (choice) {
+                case 1: {
+                    System.out.println(data);
+                    break;
+                }
+                case 2: {
+                    sellDisplay();
+                    break;
+                }
+                default:
+                    break;
+             }
+         } while (choice != 0);
     }
+    
+    /**
+     * Displays a sell
+     */
+    public void sellDisplay(){}
 }
